70cce227e2f058613d5c32ee831b06aabf30f7bc
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-03-03 19:06:18 +0200

    C-reading-list

--- exercises/C-reading-list/script.js
+++ exercises/C-reading-list/script.js
@@ -1,5 +1,29 @@
 function readingList(books) {
   // Write your code here...
+    const positionForParagraph = document.getElementById('content');
+  // console.log(positionForParagraph);
+  
+  let unorderedListForBooks = document.createElement('ul');
+  
+  books.forEach(book => {
+    
+    let titleAndAuthor = document.createElement('p');
+    // console.log(`${book.title} written by ${book.author}`);
+    titleAndAuthor.innerText = `${book.title} written by ${book.author}`;
+    
+    let listedItem = document.createElement('li');
+    // console.log(listedItem);
+    
+    let imageItem = document.createElement('img');
+    console.log(imageItem);
+    // imageItem.style = background-color
+    
+    positionForParagraph.appendChild(titleAndAuthor);  
+    positionForParagraph.appendChild(unorderedListForBooks);  
+    unorderedListForBooks.appendChild(listedItem);
+    listedItem.appendChild(imageItem);
+  });  
+
 }
 
 const books = [
