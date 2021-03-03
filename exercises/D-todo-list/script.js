198a87ad10fc7c314682768b7e82081ce0899952
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-03-03 20:40:21 +0200

    D-todo-list

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,5 +1,31 @@
 function todoList(todos) {
   // Write your code here...
+  const targetDiv = document.getElementById('content');
+  console.log('this is my target', targetDiv);
+
+  let ulForList = document.createElement('ul');
+  console.log('This is my ul', ulForList);
+
+  todos.forEach(element => {
+    
+    let liForItems = document.createElement('li');
+    console.log('This is my li', liForItems);    
+    liForItems.innerText = element.todo;
+
+    if (liForItems.addEventListener('click', addLineThrough));
+      function addLineThrough () {
+        liForItems.style.setProperty('text-decoration', 'line-through');
+    } 
+    // (liForItems.removeaddEventListener('click', removeLineThrough));
+    //   function removeLineThrough () {
+    //     liForItems.style.setProperty('text-decoration', 'none');
+    //   }
+      
+    
+    ulForList.appendChild(liForItems);
+    targetDiv.appendChild(ulForList);
+  });
+
 }
 
 const todos = [
