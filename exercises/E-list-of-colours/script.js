ba32b48d73f612787503c60613c85fc53cd1f05d
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-03-03 22:08:15 +0200

    E-list-of-colours

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,5 +1,30 @@
 function listOfColours(colours) {
   // Write your code here...
+  const targetDiv = document.getElementById('content');
+  console.log('This is my target div', targetDiv);
+
+  let selectElement = document.createElement('select');
+  targetDiv.appendChild(selectElement);
+  
+  colours.forEach(element => {
+    console.log('Element is pointing to ', element);
+    
+    let paragraphElement = document.createElement('p');
+    paragraphElement.innerText = element;
+
+    let optionElement = document.createElement('option');
+
+    optionElement.addEventListener('click', colorMatch);
+    function colorMatch() {
+      optionElement.children.style.color = element;
+    }
+    
+    
+    
+    selectElement.appendChild(optionElement);
+    optionElement.appendChild(paragraphElement);
+  });
+
 }
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
