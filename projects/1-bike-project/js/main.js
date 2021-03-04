7a1ef3c3aa5d386c228f6a50f0291eae1475674c
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-03-04 19:19:59 +0200

    1-bike-project-part1c

--- projects/1-bike-project/js/main.js
+++ projects/1-bike-project/js/main.js
@@ -1,4 +1,4 @@
-//-----part1-----
+//-----part1a-----
 const targetElementBlue = document.getElementById('blueBtn');
 console.log('This is my target element', targetElementBlue);
 
@@ -16,10 +16,10 @@ function blueButtonTheme() {
     jumbotronBlue[0].style.backgroundColor = '#588fbd';
     donateBlue[0].style.backgroundColor = '#ffa500';
     volunteerBlue[0].style.backgroundColor = 'black';
-    volunteerBlack[0].style.color = 'white';    
+    volunteerBlue[0].style.color = 'white';    
 }
 
-//-----part2-----
+//-----part1b-----
 const targetElementOrange = document.getElementById('orangeBtn');
 console.log('This is my target element', targetElementOrange);
 
@@ -34,20 +34,40 @@ console.log('volunteerOrange', volunteerOrange);
 
 targetElementOrange.addEventListener('click', orangeButtonTheme);
 function orangeButtonTheme() {
-    jumbotronOrange[0].style.backgroundColor = '#5751fd';
-    donateOrange[0].style.backgroundColor = '#ffa500';
-    volunteerOrange[0].style.backgroundColor = 'black';
+    jumbotronOrange[0].style.backgroundColor = '#f0ad4e';
+    donateOrange[0].style.backgroundColor = '#5751fd';
+    volunteerOrange[0].style.backgroundColor = '31b0d5';
     volunteerOrange[0].style.color = 'white';    
+}
+
+//-----part1c-----
+const targetElementgGreen = document.getElementById('greenBtn');
+console.log('This is my target element', targetElementgGreen);
+
+let jumbotronGreen = document.getElementsByClassName('jumbotron');
+console.log('jumbotronGreen', jumbotronGreen);
+
+let donateGreen = document.getElementsByClassName('btn btn-primary btn-lrg');
+console.log('donateGreen', donateGreen);
+
+let volunteerGreen = document.getElementsByClassName('btn btn-secondary btn-lrg');
+console.log('volunteerGreen', volunteerGreen);
+
+targetElementgGreen.addEventListener('click', greenButtonTheme);
+function greenButtonTheme() {
+    jumbotronGreen[0].style.backgroundColor = '#87ca8a';
+    donateGreen[0].style.backgroundColor = '#5751fd';
+    volunteerGreen[0].style.backgroundColor = '#8c9c08';
+    volunteerGreen[0].style.color = 'white';    
 }
 
 /*
 When clicking orange it should change:
-
 Jumbotron background color to #f0ad4e
 Donate a bike button background color to #5751fd
 Volunteer button background color to #31b0d5 and text color to white
-When clicking green it should change:
 
+When clicking green it should change:
 Jumbotron background color to #87ca8a
 Donate a bike button background color to black
 Volunteer button background color to #8c9c08
