0e49cb16da2fd4f3ab35bd42f5a01f6f7dd5a662
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-03-15 21:19:34 +0200

    add event listener

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,36 +1,43 @@
 function listOfColours(colours) {
   // Write your code here...
   const targetDiv = document.getElementById('content');
-  // console.log('This is my target div', targetDiv);
-
-  
-  let paragraphElement = document.createElement('p');
-  paragraphElement.innerText = 'selectAColor';
-  
-  targetDiv.appendChild(paragraphElement);
-  
+  // console.log('This is my target div', targetDiv); 
+ 
   let selectElement = document.createElement('select');
-  targetDiv.appendChild(selectElement);
   
-  selectElement.addEventListener('click', colorMatch);
-  function colorMatch() {
-      selectElement.children.style.color = element;
-  }
   
   colours.forEach(element => {
-    // console.log('Element is pointing to ', element);
+    console.log('Element is pointing to ', element);
     
-
     let optionElement = document.createElement('option');
-    optionElement.innerText = element;
-
+    optionElement.innerHTML = element;
     
+    let paragraphElement = document.createElement('p');
+    paragraphElement.innerHTML = `You have selected: ${element}`;
     
-    
+    optionElement.addEventListener('click', colorMatch);
+    function colorMatch() {
+      // console.log('Element is pointing to ', optionElement);
+      
+      // paragraphElement.style.backgroundColor = 'element';
+      
+    }
+    console.log(paragraphElement.style.color = element);
+    console.log(paragraphElement);
+    targetDiv.appendChild(paragraphElement);
+    targetDiv.appendChild(selectElement);
     selectElement.appendChild(optionElement);
-  });
+    
+    
+    
+    
+    
+    
+    
+  }); //(element = 0; element < colours.length; element++) {
+  };
 
-}
+
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
 
