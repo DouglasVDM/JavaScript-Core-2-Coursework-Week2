b739f667f3c3848f0ef32c08280a01cd57c833ac
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-03-20 15:01:05 +0200

    1

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -4,39 +4,60 @@ function listOfColours(colours) {
   // console.log('This is my target div', targetDiv); 
  
   let selectElement = document.createElement('select');
+  targetDiv.appendChild(selectElement);
   
+  let paragraphElement = document.createElement('p');
+  paragraphElement.textContent = `'You have selected: ${selectElement.value}'`;
+  targetDiv.appendChild(paragraphElement);
   
   colours.forEach(element => {
     console.log('Element is pointing to ', element);
     
     let optionElement = document.createElement('option');
-    optionElement.innerHTML = element;
+    optionElement.textContent = element;
+    selectElement.appendChild(optionElement);
     
-    let paragraphElement = document.createElement('p');
-    paragraphElement.innerHTML = `You have selected: ${element}`;
     
     optionElement.addEventListener('click', colorMatch);
     function colorMatch() {
       // console.log('Element is pointing to ', optionElement);
       
-      // paragraphElement.style.backgroundColor = 'element';
+      paragraphElement.style.backgroundColor = 'element';
       
     }
     console.log(paragraphElement.style.color = element);
     console.log(paragraphElement);
-    targetDiv.appendChild(paragraphElement);
-    targetDiv.appendChild(selectElement);
-    selectElement.appendChild(optionElement);
-    
-    
-    
-    
-    
-    
     
   }); //(element = 0; element < colours.length; element++) {
   };
+/*
+function listOfColours(colours){
+   const targetDiv = document.getElementById("content");
+   console.log("This is my targetDiv",targetDiv);
+    let selectEl = document.createElement("select");
+    targetDiv.appendChild(selectEl);
+    //parent.appendChild(child)
+    let pEl = document.createElement("p");
+      targetDiv.appendChild(pEl);
+    pEl.textContent = "You have selected: " ;
+    colours.forEach(colourName => {
+      console.log("This is my colour in the array",colourName);
+      let colourDrop = document.createElement("option");
+      colourDrop.textContent = colourName;
+      selectEl.appendChild(colourDrop);
+      selectColour = function(){
+      pEl.style.color = selectEl.value;
+    }
+    });
+    //Complete final step in the click event to add the name of the 
+    //selected color to the p element textContent.
+    selectEl.addEventListener("click", selectColour);
+ }
 
+
+
+
+*/
 
 
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
